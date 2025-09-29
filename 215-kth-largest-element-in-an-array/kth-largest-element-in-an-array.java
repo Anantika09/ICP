7a01cd934838a5d44def9f29b77f8ac5fba6a78d
Anantika09 <anantikaag06@gmail.com>
2025-09-29 10:17:33 +0530

    Update kth-largest-element-in-an-array.java

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findKthLargest(int[] a, int k) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>();//min heap
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
         for(int i=0;i<a.length;i++){
             pq.add(a[i]);
             while(pq.size()>k){
